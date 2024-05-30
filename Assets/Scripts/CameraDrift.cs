4f8b18624f92eba4e78764fe745f55fb79ae705f
Robert Lesser <rdlesser@gmail.com>
2024-05-30 20:37:14 +0300

    Camera drift effect in main menu

---
+++ Assets/Scripts/CameraDrift.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace DefaultNamespace {
+
+    public class CameraDrift : MonoBehaviour {
+
+        [SerializeField] private Vector2 _min;
+        [SerializeField] private Vector2 _max;
+        [SerializeField] private Vector2 _yRotationRange;
+        [SerializeField] private float _lerpSpeed = 0.05f;
+        
+        private Vector3 _newPosition;
+        private Quaternion _newRotation;
+
+        private void Awake() {
+
+            var cameraTransform = transform;
+            _newPosition = cameraTransform.position;
+            _newRotation = cameraTransform.rotation;
+        }
+
+        private void Update() {
+
+            Transform cameraTransform;
+            (cameraTransform = transform).position = Vector3.Lerp(transform.position, _newPosition, Time.deltaTime * _lerpSpeed);
+            transform.rotation = Quaternion.Lerp(cameraTransform.rotation, _newRotation, Time.deltaTime * _lerpSpeed);
+
+            if (Vector3.Distance(transform.position, _newPosition) < 1f) {
+
+                GetNewPosition();
+            }
+        }
+
+        private void GetNewPosition() {
+
+            var xPos = Random.Range(_min.x, _max.x);
+            var zPos = Random.Range(_min.y, _max.y);
+            _newRotation = Quaternion.Euler(0, Random.Range(_yRotationRange.x, _yRotationRange.y), 0);
+            _newPosition = new Vector3(xPos, 0, zPos);
+        }
+
+    }
+
+}
