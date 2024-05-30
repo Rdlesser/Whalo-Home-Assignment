c53b49df6394c4ae2c7546827108fc3ca9e25e60
Robert Lesser <rdlesser@gmail.com>
2024-05-30 16:18:09 +0300

    main menu quit functionality scripts

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Scripts;
+using UnityEditor;
+using UnityEngine;
+
+namespace DefaultNamespace {
+
+    public class MainMenuController : MonoBehaviour{
+
+        [SerializeField] private MainMenuView _mainMenuView;
+        
+        private ServiceReceiver<ISceneService> _sceneService = new();
+
+        private void Start() {
+
+            _mainMenuView.OnMenuOptionClicked += HandleMenuOptionClick;
+        }
+
+        private void HandleMenuOptionClick(MenuOption menuOption) {
+
+            switch (menuOption) {
+
+
+                case MenuOption.StartGame:
+                    StartGame();
+                    break;
+
+                case MenuOption.Events:
+                    GoToEventsScreen();
+                    break;
+
+                case MenuOption.Quit:
+                    QuitGame();
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(menuOption), menuOption, null);
+            }
+        }
+
+        private void StartGame() {
+            
+            _sceneService.Get().MoveToScene(SceneName.Game);
+        }
+
+        private void GoToEventsScreen() {
+            throw new NotImplementedException();
+        }
+
+        private void QuitGame() {
+
+#if UNITY_EDITOR
+            EditorApplication.Exit(0);
+            return;
+#endif
+            Application.Quit();
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/MainMenuView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts {
+
+    public class MainMenuView : MonoBehaviour {
+
+        [SerializeField] private Button _playButton;
+        [SerializeField] private Button _eventsButton;
+        [SerializeField] private Button _quitButton;
+
+        public Action<MenuOption> OnMenuOptionClicked;
+
+        private void Start() {
+            
+            _quitButton.onClick.AddListener(ReactToQuitClick);
+        }
+
+        private void ReactToQuitClick() {
+
+            OnMenuOptionClicked?.Invoke(MenuOption.Quit);
+        }
+    }
+
+    public enum MenuOption {
+
+        StartGame,
+        Events,
+        Quit
+    }
+
+}
