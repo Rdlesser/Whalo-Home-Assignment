c0d2f56792e8bbdc8b4621a1f92ae5fedb4f7780
Robert Lesser <rdlesser@gmail.com>
2024-06-01 22:43:08 +0300

    PopupController and view started work

---
+++ Assets/Scripts/PopupController.cs
@@ -0,0 +1,17 @@
+﻿using Cysharp.Threading.Tasks;
+using DefaultNamespace;
+using UnityEngine;
+
+public class PopupController : Controller{
+
+        [SerializeField] private PopupView _popupView;
+
+        protected override UniTask Initialize() {
+                throw new System.NotImplementedException();
+        }
+
+        protected override void Clean() {
+                throw new System.NotImplementedException();
+        }
+
+}
---
+++ Assets/Scripts/PopupView.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace {
+
+    public class PopupView : MonoBehaviour{
+
+        
+
+    }
+
+}
